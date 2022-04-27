8c600ac4e246da3af0e121b97df9a818078a26cd
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2022-04-26 21:29:33 -0300

    Initial commit

---
+++ src/main/java/com/example/projetofinal_clinica_estetica/ProjetoFinalClinicaEsteticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetofinal_clinica_estetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalClinicaEsteticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFinalClinicaEsteticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetofinal_clinica_estetica/ProjetoFinalClinicaEsteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetofinal_clinica_estetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalClinicaEsteticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
