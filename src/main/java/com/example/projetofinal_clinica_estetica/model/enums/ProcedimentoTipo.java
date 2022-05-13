3bfee510086116f9fa4a5dbf9740303fdb191552
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2022-05-13 16:33:55 -0300

    Create model enum Procedimento

---
+++ src/main/java/com/example/projetofinal_clinica_estetica/model/enums/ProcedimentoTipo.java
@@ -0,0 +1,8 @@
+package com.example.projetofinal_clinica_estetica.model.enums;
+
+public enum ProcedimentoTipo {
+    LIMPEZA_DE_PELE,
+    HARMONIZACAO_FACIAL,
+    DRENAGEM,
+    APLICACAO_BOTOX
+}
