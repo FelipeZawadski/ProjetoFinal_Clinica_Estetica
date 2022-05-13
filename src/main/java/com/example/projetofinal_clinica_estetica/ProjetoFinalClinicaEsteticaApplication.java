d83fd8d7a0dbe229962ae09267128b4d79c853de
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2022-05-13 16:38:41 -0300

    Alterações

--- src/main/java/com/example/projetofinal_clinica_estetica/ProjetoFinalClinicaEsteticaApplication.java
+++ src/main/java/com/example/projetofinal_clinica_estetica/ProjetoFinalClinicaEsteticaApplication.java
@@ -1,7 +1,10 @@
 package com.example.projetofinal_clinica_estetica;
 
+import com.example.projetofinal_clinica_estetica.model.Paciente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ProjetoFinalClinicaEsteticaApplication {
--- src/main/java/com/example/projetofinal_clinica_estetica/model/Paciente.java
+++ src/main/java/com/example/projetofinal_clinica_estetica/model/Paciente.java
@@ -1,12 +1,12 @@
 package com.example.projetofinal_clinica_estetica.model;
 
-import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
 
