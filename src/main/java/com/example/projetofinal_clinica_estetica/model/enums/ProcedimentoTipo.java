7e75f1b81c42b2b255fa71f43fc5c264d27803e8
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2022-07-01 14:53:25 -0300

    Remoção Classe

--- src/main/java/com/example/projetofinal_clinica_estetica/model/enums/ProcedimentoTipo.java
+++
@@ -1,8 +0,0 @@
-package com.example.projetofinal_clinica_estetica.model.enums;
-
-public enum ProcedimentoTipo {
-    LIMPEZA_DE_PELE,
-    HARMONIZACAO_FACIAL,
-    DRENAGEM,
-    APLICACAO_BOTOX
-}
